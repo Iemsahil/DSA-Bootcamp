a51d21d6b85af591904fed8c52bb453d7222799e
MD Sahil <ahmadsahil192@gmail.com>
2023-01-23 13:42:55 +0530

    Add files via upload

---
+++ 2.conditions-and-loop/code/src/com/sahil/CaseCheck.java
@@ -0,0 +1,22 @@
+package com.sahil;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+        System.out.println(ch);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        }
+//        else if (ch >= 'A' && ch <= 'Z') {
+//            System.out.println("Uppercase");
+//            OR
+        else {
+            System.out.println("Uppercase");
+
+        }
+    }
+}
