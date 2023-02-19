ec62bc4616b49d04769bd5ea46b217db57c85315
MD Sahil <ahmadsahil192@gmail.com>
2023-02-19 19:39:16 +0530

    Add files via upload

--- 6-Linear Search/code/src/com/sahil/Main.java
+++ 6-Linear Search/code/src/com/sahil/Main.java
@@ -2,13 +2,47 @@ package com.sahil;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {23, 45, 76, 90, 12, 19};
+        int[] nums = {23, 45, 76, 90, 12, 19, 90, 12, -98};
         int target = 19;
         int ans = linearSearch(nums, target);
         System.out.println(ans);
 
     }
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
 
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return false;
+    }
+
+    // search the target and return the element
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return Integer.MAX_VALUE;
+    }
     // search in the array: return the index if item found
     // otherwise if item not found return -1
     static int linearSearch(int[] arr, int target) {
